306240c177c85fe965d89be4d4afb2d0ae2f33ee
unknown <naufalhmuzakki@gmail.com>
2024-10-07 09:28:48 +0700

    selection sort

---
+++ array/sort/selectionSort.py
@@ -0,0 +1,20 @@
+testCase = [2, 3, 20, 4, 6, 2, 1, 4, 693, 31, 4, 4]
+
+def selectionSort(array) :
+    arraySize = len(array)
+    
+    for i in range(arraySize):
+        
+        smallest = i
+        for j in range(i+1, arraySize):
+            if array[j] < array[smallest]:
+                smallest =j
+            
+                
+        if smallest != i:
+            array[i], array[smallest] =  array[smallest],  array[i]
+    
+    return array
+
+
+print(selectionSort(testCase))
