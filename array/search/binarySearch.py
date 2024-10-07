283da84028e744ef598a476b3408c12c700a804b
unknown <naufalhmuzakki@gmail.com>
2024-10-07 09:05:13 +0700

    array search

---
+++ array/search/binarySearch.py
@@ -0,0 +1,24 @@
+
+#array need to be sorted
+sortedArray = [1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+target = 12
+
+
+def binarySearch(sortedArray : list, target : int) -> int:
+    left : int = 0;
+    right : int = len(sortedArray) - 1
+    
+    while(left <= right):    
+        mid : int = left + (right - left) // 2
+        
+        if(sortedArray[mid] == target):
+            return mid
+        elif(sortedArray[mid] < target):
+            left = mid+1;
+        elif(sortedArray[mid] > target):
+            right = mid - 1;
+    
+    return -1
+
+
+print(binarySearch(sortedArray, target))
---
+++ array/search/sequentialSearch.py
@@ -0,0 +1,16 @@
+
+
+array = [2, 3, 20, 4, 6, 2, 1, 4, 693, 31, 4, 4]
+target = 31
+
+def sequentialSearch(array : list, target : int)-> int:
+    for i, number in enumerate(array):
+        if number == target:
+            return i
+    return -1
+
+
+print(sequentialSearch(array,target))
+
+
+    
